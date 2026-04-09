b68de561f0b291910684086ec29d1274d2f01694
J-Co-de <jclark3987@gmail.com>
2026-04-08 22:34:15 -0500

    fix: update header structure and styles for improved layout and responsiveness

--- script.js
+++
@@ -1,9 +0,0 @@
-function lockHeight() {
-  document.documentElement.style.setProperty(
-    '--vh',
-    `${window.innerHeight * 0.01}px`
-  );
-}
-
-window.addEventListener('resize', lockHeight);
-lockHeight();
