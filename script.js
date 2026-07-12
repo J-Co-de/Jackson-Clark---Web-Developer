88b59f0ad472095b20db5e0cee4819f6c717c10d
J-Co-de <jclark3987@gmail.com>
2026-07-11 20:04:49 -0500

    style: enhance button color and add transition effect to navigation bar

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const modal = document.getElementById("disclaimer-modal");
   const closeBtn = document.getElementById("close-modal");
 
-  closeBtn.onclick = function() {
+  closeBtn.onclick = function () {
     modal.style.display = "none";
   };
 });
+if (window.innerWidth < window.innerHeight) {
+  let lastScrollY = window.scrollY;
 
+  window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+
+    if (window.scrollY > lastScrollY) {
+      // scrolling down
+      nav.classList.add("hide");
+    } else {
+      // scrolling up
+      nav.classList.remove("hide");
+    }
+    lastScrollY = window.scrollY;
+  });
+}
